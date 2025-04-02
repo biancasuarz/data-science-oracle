5694d23022c872cf6a9d4c5f7f7521e6d329d777
biancasuarz <blancasuarz@gmail.com>
2025-04-02 20:36:22 -0300

    fix: global user

--- modulo01/aula02/variavel.py
+++ modulo01/aula02/variavel.py
@@ -6,8 +6,11 @@ print(type(nome_aluno))
 idade_aluno = 20
 print(type(idade_aluno))
 
-media_aluno = 9.4
+media_aluno = 9.5
 print(type(media_aluno))
 
 situacao_aprovacao = True 
 print(type(situacao_aprovacao))
+
+print('Aprovada!')
+ 
