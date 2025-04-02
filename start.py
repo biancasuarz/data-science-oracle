a484fd88c2a44dd1a094fade6c16ca9d2217f8d7
user <user@users-MacBook-Pro.local>
2025-04-02 20:00:43 -0300

    Utilizando print - primeira aula

---
+++ start.py
@@ -0,0 +1,22 @@
+#Imprima a frase Escola de Dados4 da Oracle!
+print('Escola de Dados da Oracle!')
+
+#Imprima seu nome e seu sobrenome seguindo a estrutura abaixo:
+nome = 'Bianca'
+sobrenome = 'Soares'
+print(nome + sobrenome)
+
+#mprima o seu primeiro nome letra a letra. 
+print('B')
+print('I')
+print('A')
+print('N')
+print('C')
+print('A')
+
+#Imprima o dia do seu nascimento em formato dia mês ano.
+# Lembrando que os valores de dia e ano não podem estar entre aspas.
+print(24, 'Fevereiro', 2000)
+
+#Imprima, em um único print, o atual ano que você está fazendo esse curso.
+print('Ano atual: ', 2025)
