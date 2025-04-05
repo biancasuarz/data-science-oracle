26571e673ee9bcf7743c99ae1f50097800b8eb03
biancasuarz <blancasuarz@gmail.com>
2025-04-04 23:42:56 -0300

    aula 02 - manipulando strings

---
+++ modulo01/aula02/strings.py
@@ -0,0 +1,16 @@
+texto = "Amo Jesus"
+print(texto)
+
+print(texto.upper())
+print(texto.lower())
+
+invertido = ''.join(reversed(texto))
+print(invertido)
+
+n1 = 123456789
+inverso = int(str(n1)[::-1])
+print(inverso)
+
+texto2 = "Byanca"
+texto2 = texto2.strip().replace('y', 'i')
+print(texto2)
