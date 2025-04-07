6fb8a53bc6d85cf631c8cb25a2741bdd29122d35
biancasuarz <blancasuarz@gmail.com>
2025-04-07 16:29:33 -0300

    ex02 - desafio02

--- modulo01/aula02/desafio_2.py
+++ modulo01/aula02/desafio_2.py
@@ -8,3 +8,14 @@ elif percentual < 0:
     print(f"Houve decrescimento de {percentual}%.")
 else:
     print("Não houve crescimento nem decrescimento.")
+
+#Escreva um programa que determine se uma letra fornecida pela pessoa usuária é uma vogal ou consoante.
+
+letra = input("Digite uma letra: ").lower()
+
+if len(letra) != 1 or not letra.isalpha():
+    print("Por favor, digite apenas uma letra do alfabeto.")
+elif letra in 'aeiou':
+    print("É uma vogal.")
+else:
+    print("É uma consoante.")
