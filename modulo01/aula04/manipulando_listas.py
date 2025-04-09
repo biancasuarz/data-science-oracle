652ce91760c251a6ef89cc3c01365dc15c089a71
biancasuarz <blancasuarz@gmail.com>
2025-04-09 11:34:09 -0300

    aula04 - manipulando listas

---
+++ modulo01/aula04/manipulando_listas.py
@@ -0,0 +1,16 @@
+raca_caes = ['Labrador Retriever',
+             'Bulldog Francês',
+             'Pastor Alemão',
+             'Poodle']
+
+#insert para adicionar indice, elemento
+raca_caes.insert(1, 'Golden Retriever')
+print(raca_caes)
+
+#pop remove determinado elemento da lista
+raca_caes.pop(1)
+print(raca_caes)
+
+#sort organiza elementos em ordem crescente ou decrescente
+raca_caes.sort()
+print(raca_caes)
