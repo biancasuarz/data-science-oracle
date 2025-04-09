e06cdfd3752fe2bd81a9cd120d2d0ac0cbf6b4cf
biancasuarz <blancasuarz@gmail.com>
2025-04-09 11:23:48 -0300

    aula 04 - usando join

---
+++ modulo01/aula04/join_lista.py
@@ -0,0 +1,10 @@
+misturas = ['Tintas: vermelho, azul e amarelo',
+            'Verde: mistura de azul e amarelo',
+            'Laranja: mistura de vermelho e amarelo',
+            'Roxo: mistura de vermelho e azul']
+unificador = '. '
+string_misturas = unificador.join(misturas)
+print(string_misturas)
+
+print(misturas)
+print(misturas[0])
